74a8049ba632b5e3114359c2f24d96d26afe1da7
bailaohe <bailaohe@gmail.com>
2020-10-31 02:03:22 +0800

    updated

--- src/parade/server/dash/dynload.py
+++ src/parade/server/dash/dynload.py
@@ -1,4 +1,5 @@
 import dash
+import dash_html_components as html
 import yaml
 
 from parade.core.context import Context
@@ -23,16 +24,49 @@ class YamlDashboard(Dashboard):
 
     def __init__(self, app: dash.Dash, context: Context):
         Dashboard.__init__(self, app, context)
-        config_dict = YamlDashboard.load_config('test.yaml')
-        layout_dict = config_dict['layout']
+        self.config_dict = self.load_config()
+        self.layout_dict = YamlDashboard.parse_rows(self.config_dict['layout'])
 
+    @property
+    def display_name(self):
+        """
+        get the display name of the dashboard
+        :return: the display name of the dashboard
+        """
+        return self.config_dict['displayName']
 
-
-    @staticmethod
-    def load_config(config_path):
-        return yaml.safe_load(open(config_path))
+    def load_config(self):
+        raise NotImplementedError("The target is required")
+        # return yaml.safe_load(open(config_path))
 
     @staticmethod
     def parse_rows(rows):
+        rows_layout = []
         for row in rows:
+            row_layout = YamlDashboard.parse_row(row['columns'])
+            rows_layout.append(html.Div(row_layout, className='parade-row'))
 
+        return rows_layout
+
+    @staticmethod
+    def parse_row(row):
+        row_layout = []
+        for col in row:
+            col_width = col['width']
+            col_type = col['type']
+            if col_type == 'widget':
+                row_layout.append(html.Div('widget', className='parade-widget ' + col_width))
+            elif col_type == 'segment':
+                row_layout.append(html.Div('segment', className=col_width))
+            elif col_type == 'row-container':
+                sub_rows_layout = YamlDashboard.parse_rows(col['rows'])
+                row_layout.append(html.Div(sub_rows_layout, className='parade-col ' + col_width))
+        return row_layout
+
+    @property
+    def layout(self):
+        """
+        get the dash-layout segment of the dashboard
+        :return: the dash-layout segment of the dashboard
+        """
+        return self.layout_dict;
